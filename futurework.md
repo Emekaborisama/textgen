8fa04910567c7b5044e9cbe3ad562180c8a67c23
Emeka Boris Ama <borisphilosophy@gmail.com>
2021-03-17 15:40:26 +0000

    007

---
+++ futurework.md
@@ -0,0 +1,1 @@
+1. Add evalution metrics for the text generation.
--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 from setuptools import find_packages, setup
-setup(
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
     name='text_gen',
     packages=find_packages(include=['text_gen']),
-    version = '0.1.0',
+    version = '0.2.0',
     description='build a text generation model',
     #long_description_content_type = 'Tensor text generation is a python library that allow you to build a text generation model',
     author='Emeka Boris Ama',
     author_email = 'borisphilosophy@gmail.com',
     license='MIT',
+    include_package_data=True,
     url='https://github.com/Emekaborisama/textgen',
-    install_requires=[],
+    #install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
 )
 
+install_requires = ['pandas', 'tensorflow', 'torchvision', 'numpy', 'matplotlib', 'keras', 'setuptools>=47.1.1']
+
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
